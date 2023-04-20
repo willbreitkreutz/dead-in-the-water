dd101a2916bfa80596474e9d68e3c5a009b2efbb
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2023-04-20 14:14:14 -0500

    save

--- src/app_components/timeseries-chart.jsx
+++ src/app_components/timeseries-chart.jsx
@@ -1,5 +1,6 @@
 import Chart from "./chart";
 import useAPI from "../app_hooks/useAPI";
+import Loader from "./loader";
 
 function TimeseriesChart({ location }) {
   const timeseries = useAPI("catalog/TIMESERIES", {
@@ -7,10 +8,32 @@ function TimeseriesChart({ location }) {
     office: location["office-id"],
   });
 
-  console.log(timeseries);
+  if (Array.isArray(timeseries)) return <Loader />;
 
   const data = [];
-  return <Chart data={data} />;
+  return (
+    <>
+      <div className="col-3">
+        <div className="form-group">
+          {timeseries.entries
+            .filter((ts) => {
+              return !!ts.extents.length;
+            })
+            .sort((a, b) => {
+              if (a.name > b.name) return 1;
+              if (a.name < b.name) return -1;
+              return 0;
+            })
+            .map((ts) => {
+              return <div key={ts.name}>{ts.name}</div>;
+            })}
+        </div>
+      </div>
+      <div className="col-9">
+        <Chart data={data} />
+      </div>
+    </>
+  );
 }
 
 export default TimeseriesChart;
